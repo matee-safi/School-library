dd02dc24982e2d8e4d1201557a2cca0d8e9cdf3a
matee-safi <matiullahsafibbc@gmail.com>
2023-06-01 16:15:52 +0430

    set up associations

--- classroom.rb
+++ classroom.rb
@@ -1,11 +1,15 @@
+require_relative 'student'
 class Classroom
-    attr_accessor :label
-    def initialize(label)
-        @label = label
-    end
-# Create the has-many/belongs-to relationship between Classroom and Student. The following should be implemented:
-# Create the has-many side (a classroom has many students).
-# Create the belongs-to side (a student belongs to a classroom).
-# Make sure that when adding a student to a classroom it also sets the classroom for the student.
-# Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
 end
